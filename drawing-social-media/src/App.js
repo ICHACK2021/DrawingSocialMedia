c3b1394a404a8e7d14e6abcf5690b5e2cb665dca
ss1234 <ss450555@gmail.com>
2021-03-27 20:57:22 +0000

    Style fix

--- drawing-social-media/src/App.js
+++ drawing-social-media/src/App.js
@@ -1,11 +1,10 @@
-import './App.css';
+import "./App.css";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
-import Login from './Pages/Login'
-import Signup from './Pages/Signup';
-import Canvas from './Pages/Canvas'
+import Login from "./Pages/Login";
+import Signup from "./Pages/Signup";
+import Canvas from "./Pages/Canvas";
 
 const App = () => {
-
   return (
     <BrowserRouter>
       <Switch>
@@ -24,17 +23,20 @@ const App = () => {
       </Switch>
     </BrowserRouter>
   );
-}
+};
 
 const sendRequest = (e) => {
   const requestOptions = {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify()
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(),
   };
-  fetch('http://localhost:5000/getpost?username=hi&picture=aaaaa', requestOptions)
-    .then(response => response.json())
-    .then(data => console.log(data));
-}
+  fetch(
+    "http://localhost:5000/getpost?username=hi&picture=aaaaa",
+    requestOptions
+  )
+    .then((response) => response.json())
+    .then((data) => console.log(data));
+};
 
 export default App;
--- drawing-social-media/src/Pages/Signup.js
+++ drawing-social-media/src/Pages/Signup.js
@@ -1,28 +1,36 @@
 import React, { useState, useRef, Component } from "react";
-import  {Button, Form }  from "react-bootstrap";
-import {Link} from "react-router-dom";
+import { Button, Form } from "react-bootstrap";
+import { Link } from "react-router-dom";
 import "./Signup.css";
 import CanvasDraw from "react-canvas-draw";
-import { SketchPicker } from 'react-color';
+import { SketchPicker } from "react-color";
 import classNames from "./canvas.css";
 
 const validateForm = (username, email, password) => {
-    let validRegex = new RegExp(/[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,15}/g)
-  return 0 < username.length && username.length <= 20 && 0 < password.length && password.length <= 20 && validRegex.test(email);
-}
+  let validRegex = new RegExp(/[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,15}/g);
+  return (
+    0 < username.length &&
+    username.length <= 20 &&
+    0 < password.length &&
+    password.length <= 20 &&
+    validRegex.test(email)
+  );
+};
 
 const handlingSubmit = (username, email, password, picture) => {
-    // Need to change functionality of this!
+  // Need to change functionality of this!
   console.trace();
-  fetch(`http://localhost:5000/login?username=${username}&password=${password}&picture=${picture}`, 
-  {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify()
-  }).then(response => response.json())
-    .then(data => console.log(data)); // data.status if 0 success else failure
-}
-
+  fetch(
+    `http://localhost:5000/login?username=${username}&password=${password}&picture=${picture}`,
+    {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(),
+    }
+  )
+    .then((response) => response.json())
+    .then((data) => console.log(data)); // data.status if 0 success else failure
+};
 
 class Signup extends Component {
   state = {
@@ -35,103 +43,119 @@ class Signup extends Component {
     brushRadius: 10,
     lazyRadius: 0,
     picture: "",
-  }
+  };
 
   handleChangeComplete = (color) => {
-      this.setState({ color: color.hex });
+    this.setState({ color: color.hex });
   };
 
   render() {
-  return (
-    <div className="Login">
-      <Form>
+    return (
+      <div className="Login">
+        <Form>
+          <Form.Group size="lg" controlId="Username">
+            <Form.Label>Username</Form.Label>
+            <Form.Control
+              autoFocus
+              type="username"
+              value={this.state.username}
+              onChange={(e) => this.setState({ username: e.target.value })}
+            />
+          </Form.Group>
 
-        <Form.Group size="lg" controlId="Username">
-          <Form.Label>Username</Form.Label>
-          <Form.Control
-            autoFocus
-            type="username"
-            value={this.state.username}
-            onChange={(e) => this.setState({ username: e.target.value})}
-          />
-        </Form.Group>
+          <Form.Group size="lg" controlId="email">
+            <Form.Label>Email</Form.Label>
+            <Form.Control
+              type="email"
+              value={this.state.email}
+              onChange={(e) => this.setState({ email: e.target.value })}
+            />
+          </Form.Group>
 
-        <Form.Group size="lg" controlId="email">
-          <Form.Label>Email</Form.Label>
-          <Form.Control
-            type="email"
-            value={this.state.email}
-            onChange={(e) => this.setState({ email: e.target.value})}
-          />
-        </Form.Group>
-
-        <Form.Group size="lg" controlId="password">
-          <Form.Label>Password</Form.Label>
-          <Form.Control
-            type="password"
-            value={this.state.password}
-            onChange={(e) => this.setState({ password: e.target.value})}
-          />
-        </Form.Group>
-        <Button size="lg" onClick={() => handlingSubmit(this.state.username, this.state.email, this.state.password, this.state.picture)} disabled={!validateForm(this.state.username, this.state.email, this.state.password, this.state.picture)}>
-          Signup
-        </Button>
-        <Link to="/login">
-          <p/>
+          <Form.Group size="lg" controlId="password">
+            <Form.Label>Password</Form.Label>
+            <Form.Control
+              type="password"
+              value={this.state.password}
+              onChange={(e) => this.setState({ password: e.target.value })}
+            />
+          </Form.Group>
+          <Button
+            size="lg"
+            onClick={() =>
+              handlingSubmit(
+                this.state.username,
+                this.state.email,
+                this.state.password,
+                this.state.picture
+              )
+            }
+            disabled={
+              !validateForm(
+                this.state.username,
+                this.state.email,
+                this.state.password,
+                this.state.picture
+              )
+            }
+          >
+            Signup
+          </Button>
+          <Link to="/login">
+            <p />
+            <div>Login</div>
+          </Link>
+        </Form>
         <div>
-            Login
-        </div>
-        </Link>
-      </Form>
-      <div>
-            <div className={classNames.tools}>
-          <button
-            onClick={() => {
-              this.saveableCanvas.clear();
-            }}
-          >
-            Clear
-          </button>
-          <button
-            onClick={() => {
-              this.saveableCanvas.undo();
-            }}
-          >
-            Undo
-          </button>
-          <div>
-            <label>Brush-Radius:</label>
-            <input
-              type="number"
-              value={this.state.brushRadius}
-              onChange={e =>
-                this.setState({ brushRadius: parseInt(e.target.value, 10) })
-              }
+          <div className={classNames.tools}>
+            <button
+              onClick={() => {
+                this.saveableCanvas.clear();
+              }}
+            >
+              Clear
+            </button>
+            <button
+              onClick={() => {
+                this.saveableCanvas.undo();
+              }}
+            >
+              Undo
+            </button>
+            <div>
+              <label>Brush-Radius:</label>
+              <input
+                type="number"
+                value={this.state.brushRadius}
+                onChange={(e) =>
+                  this.setState({ brushRadius: parseInt(e.target.value, 10) })
+                }
+              />
+            </div>
+          </div>
+          <div className="rowColour">
+            <div>
+              <CanvasDraw
+                ref={(canvasDraw) => (this.saveableCanvas = canvasDraw)}
+                brushColor={this.state.color}
+                brushRadius={this.state.brushRadius}
+                lazyRadius={this.state.lazyRadius}
+                canvasWidth={this.state.width}
+                canvasHeight={this.state.height}
+                onChange={() =>
+                  this.setState({ picture: this.saveableCanvas.getSaveData() })
+                }
+              />
+            </div>
+            <SketchPicker
+              color={this.state.color}
+              onChangeComplete={this.handleChangeComplete}
             />
           </div>
         </div>
-            <div className="rowColour">
-            <div>
-        <CanvasDraw
-          ref={canvasDraw => (this.saveableCanvas = canvasDraw)}
-          brushColor={this.state.color}
-          brushRadius={this.state.brushRadius}
-          lazyRadius={this.state.lazyRadius}
-          canvasWidth={this.state.width}
-          canvasHeight={this.state.height}
-          onChange={() => this.setState({picture: this.saveableCanvas.getSaveData()})}
-        />
-        </div>
-        <SketchPicker
-            color={ this.state.color }
-            onChangeComplete={ this.handleChangeComplete }
-          />
-        </div>
-        </div>
-    </div>
-  );
+      </div>
+    );
+  }
 }
-}
-
 
 export default Signup;
